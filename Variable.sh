588e848a77993a0d5e7314e4766a81ef81751d41
Anvesh Kartan <anveshkartan23@gmail.com>
2025-09-24 00:57:58 +0530

    conversation

---
+++ Variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Sachin
+PERSON2=Shravan
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: I'm good $PERSON1. How are you?"
+echo "$PERSON1:: I'm good thanks! What are you learning these days?"
+echo "$PERSON2:: I'm learning shell scripting."
