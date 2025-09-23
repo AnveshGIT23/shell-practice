7a6821789b45cf2993fceea6314514b3d0522894
Anvesh Kartan <anveshkartan23@gmail.com>
2025-09-24 00:40:59 +0530

    conversation

---
+++ Conversation.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Conversation between Anvesh and Akhil.
+
+echo "Anvesh:: Hi Akhil, How are you?"
+echo "Akhil:: I'm good Anvesh. How are you?"
+echo "Anvesh:: I'm good thanks! What are you learning these days?"
+echo "Akhil:: I'm learning shell scripting."
