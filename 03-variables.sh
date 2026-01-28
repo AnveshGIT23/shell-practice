e5e85630176368873aac738d46a7975867b5dfb4
Anvesh Kartan <anveshkartan23@gmail.com>
2026-01-28 14:18:25 +0530

    conversation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=John
+PERSON2=Raheem
+
+echo "$PERSON1:: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hello R$PERSON1, I'm fine. How are you?"
+echo "$PERSON1:: I'm good thank you! So, what are you learning recently"
+echo "$PERSON2:: I'm learning devsecops"
