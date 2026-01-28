ad1749a13aa631db0251d7d3834e9e2c5b9c43ac
Anvesh Kartan <anveshkartan23@gmail.com>
2026-01-28 14:01:47 +0530

    conversation

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh:: Hi Suresh, How are you?"
+echo "Suresh:: Hello Ramesh, I'm fine. How are you?"
+echo "Ramesh:: I'm good thank you! So, what are you learning recently"
+echo "Suresh:: I'm learning devsecops"
