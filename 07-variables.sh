c244d275a01c0c963b1e0206cfa98b194047b88d
Anvesh Kartan <anveshkartan23@gmail.com>
2026-01-31 14:22:01 +0530

    convo

--- 07-variables.sh
+++ 07-variables.sh
@@ -4,3 +4,6 @@ echo "All variables passed to this script: $@"
 echo "All variables passed to this script: $*"
 
 echo "script name: $0"
+echo "current directory: $pwd"
+echo "who is running this: $user"
+echo "Home directory of the user: $home"
